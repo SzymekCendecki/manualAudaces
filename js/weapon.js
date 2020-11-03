cab5ab939fb1dc72f1b1c341e73f0118b3597fc6
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2020-11-03 21:01:47 +0100

    Add files via upload

--- js/weapon.js
+++ js/weapon.js
@@ -13,6 +13,10 @@ const shortPl = "Broń krótka to wszystkie rodzaje noży, sztyletów, kozików,
 
 const oneHandPl = "Broń jednoręczna: miecze, topory, młoty. Jest podstawową bronią wszystkich profesji związanych z walką. Broń o długości 80 - 100 cm.";
 
-const siegePl = "Machiny oblężnicze to wszelkiej maści kusze wałowe, balisty, katapulty, trebusze czy wieże oblężnicze. Do ich zbudowania oraz obsługi potrzebni są wykwalifikowani rzemieślnicy oraz specjalnie przeszkolona obsługa. Bez takiego sprzętu oraz osób nie sposób skutecznie prowadzić oblężenia oraz bronić murów. Mogą strzalać ogromnymi bełtami, kamiennymi kulami a czasem się zdarza, że truchłami padłych zwierząt."
+const distancePL = "Broń dystansowa to broń strzelająca. Są to łuki, kusze, proce oraz niektóre bronie specjalne (np. strzałki lub 'gwiazdki'). Bronią dystansową są także niektóre bronie oblężnicze: kusze wałowe, katapulty trebusze itd."
 
-export const weaponTypesPl = [shortPl, oneHandPl, siegePl];
+const siegePl = "Machiny oblężnicze to wszelkiej maści kusze wałowe, balisty, katapulty, trebusze czy wieże oblężnicze. Do ich zbudowania oraz obsługi potrzebni są wykwalifikowani rzemieślnicy oraz specjalnie przeszkolona obsługa. Bez takiego sprzętu oraz osób nie sposób skutecznie prowadzić oblężenia oraz bronić murów. Mogą strzalać ogromnymi bełtami, kamiennymi kulami a czasem się zdarza, że truchłami padłych zwierząt.";
+
+const magicPL = "Bronią magiczną jest każda broń, która została w jakiś sposób umagiczniona, czyli nabrała cech które zwiększaja jej możliwości. Najczęściej dzieje się to za pomocą zaklęć magów lub broń została stworzona z materiałów magicznych. Cechy magicznej broni uzależnione są od użytych magicznych materiałów, zaklęć oraz umiejętności osoby tworzącej taką broń.";
+
+export const weaponTypesPl = [shortPl, oneHandPl, distancePl, siegePl, magicPL];
