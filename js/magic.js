4d7ca9ff8b0fc46f03f7b42babbe742deab59434
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2020-11-07 12:29:28 +0100

    Add files via upload

---
+++ js/magic.js
@@ -0,0 +1,11 @@
+export const magicPl = {
+    "description":"opis",
+    "fire":"ogień",
+    "earth":"ziemia",
+    "water":"woda",
+    "air":"powietrzes"
+}
+
+const descriptionPl = "Magia podzielona jest na cztery szkoły (żywioły): ognia, ziemii, wody oraz powietrza. Poziomy każdej szkoły magii to:  adpet, mag, arcymag i mistrz. Im potężniejszy czar, tym mag musi posiadać wyższy poziom wtajemniczenia. Można używać także zaklęć innej szkoły, jednakże czary takie muszą być z poziomu niższego niż poziom szkoły głównej rzucającego czar. Gdy mag osiągnie poziom mistrzowski także w dodatkowej szkole magii, może rzucać czary bez ograniczeń. Jeżeli mag osiągnie mistrzstwo we wszystkich czterech szkołach magii, staje się 'Panem Żywiołów'. Potwierdzone jest tylko kilka takich przypadków na przestrzeni kilkuset lat. Były to głównie elfy i o dziwo jeden goblin.";
+
+const magicSchoolsPl = [descriptionPl];
