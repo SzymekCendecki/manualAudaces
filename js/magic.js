f0f45f35a34e4bb488cc9063de8583389b9acb69
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2020-11-09 21:46:57 +0100

    Add files via upload

--- js/magic.js
+++ js/magic.js
@@ -6,14 +6,16 @@ export const magicPl = {
     "air":"powietrzes"
 }
 
-const descriptionPl = "Magia podzielona jest na cztery szkoły (żywioły): ognia, ziemii, wody oraz powietrza. Poziomy każdej szkoły magii to:  adpet, mag, arcymag i mistrz. Im potężniejszy czar, tym mag musi posiadać wyższy poziom wtajemniczenia. Można używać także zaklęć innej szkoły, jednakże czary takie muszą być z poziomu niższego niż poziom szkoły głównej rzucającego czar. Gdy mag osiągnie poziom mistrzowski także w dodatkowej szkole magii, może rzucać czary bez ograniczeń. Jeżeli mag osiągnie mistrzstwo we wszystkich czterech szkołach magii, staje się 'Panem Żywiołów'. Potwierdzone jest tylko kilka takich przypadków na przestrzeni kilkuset lat. Były to głównie elfy i o dziwo jeden goblin.";
+const descriptionPl = "Oznaką przyznależności do profesji czarodziei jest kostur, wyznacznik jego statusu oraz mocy. Na początku 'kariery' adept dostaje kostur zwykły, prrosty drewniany kostur. Wraz z podnoszeniem swoich umiejętności mag zdobywa także możliwość 'ulepszenia' swojego kosturu. Zwyczajowo przyjęło się obsadzanie klejnotów na jego końcu, rzeźbienie oraz okucia z metali szlachetnych. Kamienie na szczycie kostura to: rubin (żywioł ognia), szmaragd (żywioł ziemii), szafir (żywioł wody), diament (żywioł powietrza). Każdy taki klejnot moze być umieszczony dopiero, gdy osoba parająca się magią osiągnie poziom arcymaga w danej szkole żywiołów. Gdy taka osoba będzie miała cztery takie stopnie oraz przejdzie specjalne szkolenie stanie się mistrzem żywiołów. Stopnie wtajemniczenia: adept, mag, arcymag, mistrz żywiołów. Na poziome adepta osoba przechodzi wstępne szkolenia z zakresu podstaw rzucania czarów, zielarstwa, astrologii, posługiwania się sprzętem laborattoryjnym, pisania i czytania (jeżeli jest taka potrzeba). ";
 
-const firePl = "Szkoła Magii Żywiołu Ognia jest szkołą czarów głównie ofensywnych. Czary: podgrzanie, rozpalenie ogniska, płonąca strzała, kula ognia, ognisty podmuch, deszcz ognia.";
+//Magia podzielona jest na cztery szkoły (żywioły): ognia, ziemii, wody oraz powietrza. Poziomy każdej szkoły magii to:  adpet, mag, arcymag i mistrz. Im potężniejszy czar, tym mag musi posiadać wyższy poziom wtajemniczenia. Można używać także zaklęć innej szkoły, jednakże czary takie muszą być z poziomu niższego niż poziom szkoły głównej rzucającego czar. Gdy mag osiągnie poziom mistrzowski także w dodatkowej szkole magii, może rzucać czary bez ograniczeń. Jeżeli mag osiągnie mistrzstwo we wszystkich czterech szkołach magii, staje się 'Panem Żywiołów'. Potwierdzone jest tylko kilka takich przypadków na przestrzeni kilkuset lat. Były to głównie elfy i o dziwo jeden goblin.
 
-const earthPl = "";
+const firePl = "Magia Żywiołu Ognia to głównie czary ofensywne. Czary: podgrzanie, rozpalenie ogniska, płonąca strzała, kula ognia, ognisty podmuch, deszcz ognia, ściana ognia, ognisty krąg. ";
 
-const waterPl = "";
+const earthPl = "Magia Żywiołu Ziemii to magia głównie obronna. Czary: trzesięnie ziemii, kamienny pancerz, stalowy pancerz, siła giganta, splątanie przez rośliny.";
 
-const airPl = "";
+const waterPl = "Magia Żywiołu Wody to magia głównie lecząca. Czary: leczenie lekkich ran, leczenie średnich ran, leczenie krytycznych ran, zmartwychwstanie, stworzenie bagna, stworzenie mgły, uleczenie choroby, wzmocnienie działania eliksiru leczącego.";
+
+const airPl = "Magia Żywiołu Powietrza to magia 'wsparcia'. Czary z tej szkoły rzucone np. na broń poprawiają umiejętności posługiwania się taką bronią. Czary: zwiększona zręczność, zwiększona inteligencja, zwiększona charyzma, ";
 
 export const magicSchoolsPl = [descriptionPl, firePl, earthPl, waterPl, airPl];
