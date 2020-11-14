8c1ad1caa6ae2a327e3bfc70482a3f6c2dd34bba
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2020-11-14 10:53:01 +0100

    Add files via upload

---
+++ js/legends.js
@@ -0,0 +1,22 @@
+export const worldPl = {
+    "description":"opis",
+    "fireLand":"Kontynent Żywiołu Ognia",
+    "earthLand":"Kontynent Żywiołu Ziemii",
+    "waterLand":"Kontynent Żywiołu Wody",
+    "airLand":"Kontynent Żywiołu Powietrza",
+    "fifthLand":"Piąty 'wspólny' Kontynent"
+}
+
+const descriptionPl = "Na początku był chaos. Z niego wyłoniło się cztery główne Żywioły nazwane później Bogami: ognia, wody, ziemii oraz powietrza. Przez eony trwała wojna Bogów o dominację nad pozostałymi. Gdy po tysiącach lat wojna nie przyniosła rozstrzygnięcia Bogowie stworzyli Ziemię, z pięcioma kontynentami. Po jednym dla każdego z Bogów oraz jeden wspólny. Każdy z czterech kontynentów naznaczony był energią danego Boga, miał symbolizować Boski Żywioł. Z wymieszania się energii tych czterech żywiołów powstała magia. Dwa olbrzymie wiry, po jednym na każdym biegunie, wytwarzają prądy morskie. Marynarze opowiadają o potężnych monstrach, krakenach, które potrafią zatopić największe okręty.";
+
+const firePl = "Vartucan - kontynent Boga ognia. Tropikalny klimat. Wzdłuż zachodniego brzegu rozciąga się wąski pas pustyni. Północną część pokrywają lasy tropikalne. Południową stanowi sawanna. Kontynent zamieszkują głównie elfy (w części północnej) i ludzie (w części południowej).";
+
+const earthPl = "Kidrano - kontynent Boga ziemii. Ten kontynent niemal w całości pokrywają góry oraz wzgórza (prawie 70%). To kraina krasnoludów, gnomów oraz niziołków. Krasnoludy i gnomy zamieszkuja kopalnie - miasta, które po także pełnia funkcję zamków. Na nielicznych nizinach zamieszkują niziołki.";
+
+const waterPl = "Sandrana - kontynent Boga wody. To najmniejszy kontynent. Głównie pokryty gęstymi lasami, z bardzo dużą ilością jezior. Wokół kontynentu znajdują się mnóstwo archipelagów, dużych wysp oraz atoli. Na tym kontynencie znajduje się tylko jedno miasto. Kontynent zamieszkują głównie gobliny, trole oraz orki. Co dziwne w jedyne miasto zdominowali ludzie. Na wyspach okalających kontynent znajduje się wiele pirackich twierdz.";
+
+const airPl = "Berost - kontynent Boga powietrza. Kontynent pokryty trawiastymi równinami oraz niewielkimi wzgórzami.";
+
+const fifthPl = "Widrun - To największy ze wszystkich kontynentów, a zarazem najbardziej zróżnicowany ze wszystkich. Posiada wszystkie cechy pozostałych. Legendy głoszą, iż kontynent był pierwszą siedzibą Bogów. Dowodem mają być ruiny świątyni, które znajdują się ponoć w środku puszczy, u żródeł największej rzeki kontynentu. Nikt jeszcze nie dotarł do tego miejsca, więc miejsce tej świątyni (jak i sama świątynia) pozostaja w sferze legend. W północnej częśi znajdują się pasma górskie. Są tam cztery twierdze - kopalnie krasnoludzkie. Wschodnia część to równiny, na której jest niewiele lasów. Ta część jest domeną koczowniczych orczych klanów. W środkowej części kontynentu pokrytej puszczą znajduje się wiele klanów goblinów. Południowa część jest pokryta dużą ilością jezior. Pośród nich znajdują się miasta ludzi. Zachodnia część to wzgórza pokryte lasami. To domena elfów.";
+
+export const legendsPl = [descriptionPl, firePl, earthPl, waterPl, airPl, fifthPl];
